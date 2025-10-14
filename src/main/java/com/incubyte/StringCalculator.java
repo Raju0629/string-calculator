46f37d0aa048566cd1fef41bc916773e8a4f2cf1
Raju0629 <raju.medisetti8226.com>
2025-10-14 23:06:10 +0530

    feat: support newlines as delimiters

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -5,10 +5,13 @@ public class StringCalculator {
         if (numbers == null || numbers.isEmpty()) return 0;
         if (!numbers.contains(",")) return Integer.parseInt(numbers);
 
+            numbers = numbers.replace("\n", ",");
+
             String[] parts = numbers.split(",");
             int sum = 0;
             for (String p : parts) sum += Integer.parseInt(p);
             return sum;
+
 
 
     }
